595adb8322ac0f563e35e1948f5848334fe8857e
unknown <sigmaligma@skibidi.com>
2024-09-12 14:50:35 +0300

    first commit

---
+++ resources/views/note/create.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <h1>create</h1>
+    <form 
+        action = "" method="POST" class="note">
+        <textarea name="note" rows="10" class="note-body" placeholder="enter your note here"></textarea>
+        <div class="note-buttons">
+            <a href="#" class="note-cancel-button">Cancel</a>
+            <button class="note-submit-button">Submit</button>
+        </div>
+
+    </form>
+</div>
---
+++ resources/views/note/show.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+    <div class="note-container single-note">
+        <div class="note-header">
+            <h1>Note:Date_goes_here</h1>
+            <div class="note-buttons">
+                <a href="#" class="note-edit-button">Edit</a>
+                <button class="note-delete-button">Delete</button>                                
+            </div>
+        </div>
+    </div class="note">
+    <div class="note-body">
+        
+    </div>
+</x-layout>
